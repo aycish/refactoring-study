1df455cf94ed16f4485c3d9966a1acd16c95460c
Unhee <88412661+aycish@users.noreply.github.com>
2023-01-09 23:50:35 +0900

    docs & feat : 냄새 2: 중복 코드 강의 내용 정리 및 예제 코드 추가 (#2)

---
+++ src/main/java/me/jounhee/refactoring/_02_duplicated_code/_06_pull_up_method/ReviewerDashboard.java
@@ -0,0 +1,9 @@
+package me.jounhee.refactoring._02_duplicated_code._06_pull_up_method;
+
+import java.io.IOException;
+
+public class ReviewerDashboard extends Dashboard {
+    public void printReviewers() throws IOException {
+        printUserNames(30);
+    }
+}
