a5e6632acbb3c7a6356626197c1a998ec06a6219
aycish <aycish07@gmail.com>
2023-03-19 17:27:47 +0900

    feat : smell13 예제 코드 추가

---
+++ src/main/java/me/jounhee/refactoring/_13_loop/_33_replace_loop_with_pipeline/Author.java
@@ -0,0 +1,25 @@
+package me.jounhee.refactoring._13_loop._33_replace_loop_with_pipeline;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Author {
+
+    private String company;
+
+    private String twitterHandle;
+
+    public Author(String company, String twitterHandle) {
+        this.company = company;
+        this.twitterHandle = twitterHandle;
+    }
+
+    static public List<String> TwitterHandles(List<Author> authors, String company) {
+        return authors.stream()
+                .filter(a -> a.company.equals(company))
+                .map(a -> a.twitterHandle)
+                .filter(handle -> handle != null)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/me/jounhee/refactoring/_13_loop/package-info.java
@@ -0,0 +1,1 @@
+package me.jounhee.refactoring._13_loop;
